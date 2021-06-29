8d3810813b61104579a530504c849011e2f190c9
blairemendoza <rumilchristianblaire.mendoza@g.msuiit.edu.ph>
2021-06-29 23:04:09 +0800

    Fix sql ddl

--- calls.py
+++ calls.py
@@ -13,14 +13,14 @@ def databaseSetup():
             Gender VARCHAR(10),
             Year_Level INTEGER NOT NULL,
             Course_Code VARCHAR(5) NOT NULL,
-            PRIMARY KEY(ID_Number)
+            PRIMARY KEY(ID_Number),
             FOREIGN KEY(Course_Code) REFERENCES Courses(Course_Code)
         );
     """)
     cur.execute("""
         CREATE TABLE IF NOT EXISTS "Courses"(
             Course_Code VARCHAR(5) PRIMARY KEY NOT NULL,
-            Course_Name VARCHAR(100) NOT NULL,
+            Course_Name VARCHAR(100) NOT NULL
         );
     """)
     conn.close()
